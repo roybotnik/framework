dfa8fe3c2c519451e756c56654353b2bcc25b390
Roy Q <roybotnik@gmail.com>
2011-10-18 00:10:14 -0400

    tests

--- test/core/router/test.js
+++ test/core/router/test.js
@@ -16,7 +16,15 @@ var suite = new testing.Suite({
 		var route = router.findRoute('/test');
 
 		testing.Assert(route !== null, 'Route was null');
-	}
+	},
+
+	'Should not find a route for /fasdf' : function () {
+		var route = router.findRoute('/fasdf');
+
+		testing.Assert(route === null, 'Route was not null');
+	},
+
+	
 });
 
 suite.run();
