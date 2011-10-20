99e28d87a19e1d45042ef16b6f922dc44b0e9aa3
Roy Q <roybotnik@gmail.com>
2011-10-19 23:37:15 -0400

    fixed some bugs in router

--- test/core/router/_config.js
+++ test/core/router/_config.js
@@ -1,4 +1,5 @@
 exports.routes = [
+	{ url: '/', method: 'get', controller: 'testController', action: 'index' },
 	{ url: '/test', method: 'get', controller: 'testController', action: 'testAction' },
 	{ url: '/skrit', method: 'post', controller: 'testController', action: 'otherAction' },
 	{ url: '/shit', method: 'get', controller: 'testController', action: 'notAnAction' },
@@ -8,13 +9,17 @@ exports.routes = [
 var testController = function () {
 };
 testController.prototype = {
+	index : function (context) {
+		console.log('index');
+		context.res.write("index body");
+	},
 	testAction : function (context) {
 		console.log('testAction');
-		return "my body";
+		context.res.write("my body");
 	},
 	otherAction : function (context) {
 		console.log('otherAction');
-		return "other body";
+		context.res.write("other body");
 	}
 };
 var controller = new testController();
