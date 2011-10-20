d2a3950d483b50fca3d18d763b4877848194c4a2
Roy Q <roybotnik@gmail.com>
2011-10-20 15:36:36 -0400

    server tweaks, router tweaks.  need to fix a failing test

--- test/_runner.js
+++ test/_runner.js
@@ -8,6 +8,6 @@ require('./core/router/basic_tests.js');
 require('./core/router/dispatch_tests.js');
 
 // Core.Http
-require('./core/http/server/test.js');
+require('./core/http/server/tests.js');
 
 // Base
